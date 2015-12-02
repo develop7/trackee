5d5c8ecac6b3cfa1e6c8a73f541c57c67851e3f6
Andrei Dziahel <develop7@develop7.info>
2015-12-02 16:42:54 +0300

    Made it actually shoot screenshots
    in other news: stackified (LTS 3.16) and added cabal-sandbox

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Gdk.Screen
+import System.Environment
+import Data.Text
+
+main :: IO()
 main = do
-  putStrLn "Hullo zebda neiba"
+  [fileName] <- getArgs
+  initGUI
+  Just screen <- screenGetDefault
+  window <- screenGetRootWindow screen
+  size <- drawableGetSize window
+  origin <- drawWindowGetOrigin window
+  Just pxbuf <- pixbufGetFromDrawable window ((uncurry . uncurry Rectangle) origin size)
+  pixbufSave pxbuf fileName "png" ([] :: [(Text, Text)])
