1144b6f44edf94f39f40c38373d7ed3098fff759
Andrei Dziahel <develop7@develop7.info>
2015-12-06 22:59:54 +0300

    Write files named according to current UTC time

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,26 @@ import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Gdk.Screen
 import System.Environment
 import Data.Text
+import Data.Time (getCurrentTime)
+import Data.Time.ISO8601 (formatISO8601)
 
 main :: IO()
 main = do
   [fileName] <- getArgs
   initGUI
+  now <- getCurrentTime
+  pxb <- getScreenBuf
+  pixbufSave pxb (renderName fileName (formatISO8601 now) "" ) "png" ([] :: [(Text, Text)])
+
+getScreenBuf :: IO Pixbuf
+getScreenBuf = do
   Just screen <- screenGetDefault
   window <- screenGetRootWindow screen
   size <- drawableGetSize window
   origin <- drawWindowGetOrigin window
   Just pxbuf <- pixbufGetFromDrawable window ((uncurry . uncurry Rectangle) origin size)
-  pixbufSave pxbuf fileName "png" ([] :: [(Text, Text)])
+  return pxbuf
+
+renderName :: String -> String -> String -> String
+renderName prefix name suffix =
+    prefix ++ "." ++ name ++ "." ++ suffix ++ ".png"
