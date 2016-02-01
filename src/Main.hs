2368f3228084f75687a5e26ea632b699acef9c39
Andrei Dziahel <develop7@develop7.info>
2016-02-01 13:17:03 +0300

    align imports

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,12 @@
 module Main where
 
 import qualified GI.Gtk as Gtk (init)
-import GI.Gdk as Gdk
-import GI.GdkPixbuf as GPxb
-import Data.Text (pack)
-import System.Environment (getArgs)
-import Data.Time (getCurrentTime)
-import Data.Time.ISO8601 (formatISO8601)
+import           GI.Gdk as Gdk
+import           GI.GdkPixbuf as GPxb
+import           Data.Text (pack)
+import           System.Environment (getArgs)
+import           Data.Time (getCurrentTime)
+import           Data.Time.ISO8601 (formatISO8601)
 
 main :: IO()
 main = do
