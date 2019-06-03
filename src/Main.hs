f6c8a1bb1a026812434ac8915941d11f17dc3161
Andrei Dziahel <develop7@develop7.info>
2019-06-03 16:11:36 +0300

    trackee: restarting it simple
    - inline the functionality first
    - add screensaver check
    - bump stackage resolver
    - rm unneeded deps
    - specify hashes for extra-deps

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Control.Concurrent.Suspend (mDelay, sDelay, suspend)
 import           Control.Concurrent.Timer   (repeatedTimer)
 import           Control.Monad              (forever)
-import qualified GI.Gdk                     as Gdk (init)
-
-import qualified Trackee.Agents           as A (Screen (..))
-import qualified Trackee.Events.Processor as E (processEvents)
-import qualified Trackee.Types            as T (Agent (..), Plug (..))
+import qualified Data.ByteString            as B (ByteString, writeFile)
+import           Data.String.Utils          (join)
+import           Data.Time                  (UTCTime, defaultTimeLocale,
+                                             formatTime, getCurrentTime)
+import           DBus                       (MethodCall (..), methodCall)
+import           DBus.Client                (call_, connectSession)
+import qualified GI.Gdk                     as Gdk (init, pixbufGetFromWindow,
+                                                    screenGetDefault,
+                                                    screenGetRootWindow,
+                                                    windowGetGeometry)
+import qualified GI.GdkPixbuf               as Gdk.PB (pixbufSaveToBufferv)
+import qualified GI.Wnck                    as Wnck (screenGetShowingDesktop)
 
 main :: IO ()
 main = do
@@ -15,12 +24,44 @@ main = do
   setupRoutine
   forever $ suspend (sDelay 1) -- main loop
 
-agents = [A.Screen]
-
-setupAgents = mapM_ T.setup agents
+setupAgents = Gdk.init []
 
 setupRoutine = do
   theRoutine -- collect data right after starting
   repeatedTimer theRoutine (mDelay 1) -- and repeat it. TODO: delay should be configurable
   where
-    theRoutine = E.processEvents agents
+    theRoutine = doRoutine
+
+doRoutine :: IO ()
+doRoutine = do
+  skipIt <- isInhibited
+  if skipIt
+    then return ()
+    else do
+      now <- getCurrentTime
+      bytes <- captureScreenshot
+      writeScreenshot now bytes
+
+isInhibited :: IO Bool
+isInhibited = do
+  client <- connectSession
+  reply <-
+    call_
+      client
+      (methodCall "/org/gnome/ScreenSaver" "org.gnome.ScreenSaver" "GetActive")
+        {methodCallDestination = Just "org.gnome.ScreenSaver"}
+  return False
+
+writeScreenshot :: UTCTime -> B.ByteString -> IO ()
+writeScreenshot now bytes = B.writeFile (renderName ["shot", renderTime now, "jpeg"]) bytes
+  where
+    renderName = join "."
+    renderTime = formatTime defaultTimeLocale "%Y%m%dT%H%M%S"
+
+captureScreenshot :: IO B.ByteString
+captureScreenshot = do
+  Just screen <- Gdk.screenGetDefault
+  window <- Gdk.screenGetRootWindow screen
+  (x, y, w, h) <- Gdk.windowGetGeometry window
+  Just pxbuf <- Gdk.pixbufGetFromWindow window x y w h
+  Gdk.PB.pixbufSaveToBufferv pxbuf "jpeg" ["quality"] ["75"]
